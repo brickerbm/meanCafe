0e5a62d877d6ed5dd2f9a24258bd588fab12b2d8
Ben Bricker <brickerbm2@vcu.edu>
2019-11-08 13:18:55 -0500

    add methods to report service

--- frontend/src/app/report.service.ts
+++ frontend/src/app/report.service.ts
@@ -9,4 +9,68 @@ export class ReportService {
   uri = 'http://localhost:4000';
 
   constructor(private http: HttpClient) { }
+
+  // CREATE
+  addReport(
+    startTime: string,
+    endTime: string,
+    userAgents: string[],
+    passed: number,
+    total: number,
+    skipped: number,
+    failed: number,
+    fixtures: string[],
+    warnings: string[]) {
+    const report = {
+      startTime,
+      endTime,
+      userAgents,
+      passed,
+      total,
+      skipped,
+      failed,
+      fixtures,
+      warnings
+    };
+    return this.http.post('${this.uri}/reports/add', report);
+  }
+  // READ
+  getReport() {
+    return this.http.get('${this.uri}/reports');
+  }
+
+  getReportById(id) {
+    return this.http.get('${this.uri}/reports/${id}');
+  }
+
+  // UPDATE
+  updateReport(
+    id,
+    startTime: string,
+    endTime: string,
+    userAgents: string[],
+    passed: number,
+    total: number,
+    skipped: number,
+    failed: number,
+    fixtures: string[],
+    warnings: string[]) {
+      const report = {
+        startTime,
+        endTime,
+        userAgents,
+        passed,
+        total,
+        skipped,
+        failed,
+        fixtures,
+        warnings
+      };
+      return this.http.post('${this.uri}/reports/update/${id}', report);
+  }
+
+  // DELETE
+  deleteReport(id) {
+    return this.http.get('${this.uri}/reports/delete/${id}');
+  }
 }
