23dc75e2e8f456103a6642e8d5165284b1647f1a
Ben Bricker <brickerbm2@vcu.edu>
2019-11-08 10:50:54 -0500

    add typescript to backend

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -2,7 +2,30 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-let Report = new Schema (
+let Report = new Schema ({
+    startTime: {
+        type: String
+    },
+    endTime: {
+        type: String
+    },
+    userAgents: {
+        type: Array
+    },
+    passed: {
+        type: Number
+    },
+    total: {
+        type: Number
+    },
+    skipped: {
+        type: Number
+    },
+    fixtures: {
+        type: Array
+    },
+    
+}
     //{field: { type: Type }, more fields}
 );
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import Config from './models/Config'
+import Report from './models/Report'
 
 const app = express();
 const router = express.Router();
@@ -9,7 +11,7 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://[server]/issues');
+mongoose.connect('mongodb://[server]/configs');
 
 const connection = mongoose.connection;
 
@@ -21,3 +23,14 @@ app.use('/', router);
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(4000, () => console.log('Express server running on port 4000'));
+
+router.route('/configs').get((req, res) => {
+    Config.find((err, configs) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(configs);
+        }
+    });
+});
