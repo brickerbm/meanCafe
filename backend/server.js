8d5ca8c169b9802aad0141c893131896975b83fd
Ben Bricker <brickerbm2@vcu.edu>
2020-01-06 15:47:34 -0500

    install more ngrx stuff

--- backend/server.js
+++ backend/server.js
@@ -5,26 +5,22 @@ import mongoose from 'mongoose';
 import Config from './models/config.model';
 import Report from './models/report.model';
 import fs from 'fs';
-// import spawn from 'child_process';
 
 var spawn = require('child_process').spawn;
-// child = spawn('testcafe');
 
 const app = express();
 const router = express.Router();
 
-let fixtures = fs.readdirSync('./testing/fixtures').files;
-
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(bodayParser.urlencoded({ extended: flase }));
 
+// Connect to database
 mongoose.connect('mongodb://127.0.0.1:27017/meancafe');
 
 const connection = mongoose.connection;
 
+// Log that connection has been established
 connection.once('open', () => {
-    // db = database;
     console.log('MongoDB database connection established successfully!');
 });
 
@@ -33,18 +29,7 @@ app.use('/', router);
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(4000, () => console.log('Express server running on port 4000'));
 
-// Create new config/report
-router.route('/configs/add').post((req, res) => {
-    let config = new Config(req.body);
-    config.save()
-        .then(config => {
-            res.status(200).json({'config': 'Added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new config');
-        });
-});
-
+// Write config file to .testcaferc.json, run "testcafe" as child process, write report.json to DB as json obj
 router.route('/config/write').post((req, res) => {
     let data = JSON.stringify(req.body);
     // console.log(data);
@@ -73,29 +58,7 @@ router.route('/config/write').post((req, res) => {
     });
 });
 
-router.route('/reports/add').post((req, res) => {
-    let report = new Report(req.body);
-    report.save()
-        .then(report => {
-            res.status(200).json({'report': 'Added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new report');
-        });
-});
-
-// Read all configs/reports/fixture file names
-router.route('/configs').get((req, res) => {
-    Config.find((err, configs) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.json(configs);
-        }
-    });
-});
-
+// Read all reports
 router.route('/reports').get((req, res) => {
     Report.find((err, reports) => {
         if (err) {
@@ -107,6 +70,7 @@ router.route('/reports').get((req, res) => {
     });
 });
 
+// Read all fixture filenames in testing/fixtures dir
 router.route('/fixtures').get((req, res) => {
     fs.readdir('./testing/fixtures', (err, items) => {
         if (err) {
@@ -117,18 +81,7 @@ router.route('/fixtures').get((req, res) => {
     });
 });
 
-// Read specific config/report
-router.route('/configs/:id').get((req, res) => {
-    Config.findById(req.params.id, (err, issue) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.json(config);
-        }
-    });
-});
-
+// Read specific report
 router.route('/reports/:id').get((req, res) => {
     Report.findById(req.params.id, (err, report) => {
         if (err) {
@@ -140,66 +93,7 @@ router.route('/reports/:id').get((req, res) => {
     });
 });
 
-// Update specific config/report
-router.route('/configs/update/:id').post((req, res) => {
-    Config.findById(req.params.id, (err, config) => {
-        if (!config) {
-            return next(new Error('Could not load Config'));
-        }
-        else {
-            config.browsers = req.body.browsers;
-            config.src = req.body.src;
-            config.reporter.name = req.body.reporter.name;
-            config.reporter.output = req.body.reporter.output;
-            config.color = req.body.color;
-            config.skipJsErrors = req.body.skipJsErrors;
-
-            config.save().then(config => {
-                res.json('Update done');
-            }).catch(err => {
-                res.status(400).send('Update failed');
-            });
-        }
-    });
-});
-
-router.route('/reports/update/:id').post((req, res) => {
-    Report.findById(req.params.id, (err, report) => {
-        if (!report) {
-            return next(new Error('Could not load Report'));
-        }
-        else {
-            report.startTime = req.body.startTime;
-            report.endTime = req.body.endTime;
-            report.userAgents = req.body.userAgents;
-            report.passed = req.body.passed;
-            report.total = req.body.total;
-            report.skipped = req.body.skipped;
-            report.failed = req.body.failed;
-            report.fixtures = req.body.fixtures;
-            report.warnings = req.body.warnings;
-
-            report.save().then(report => {
-                res.json('Update done');
-            }).catch(err => {
-                res.status(400).send('Update failed');
-            });
-        }
-    });
-});
-
-// Delete specific config/report
-router.route('/configs/delete/:id').get((req, res) => {
-    Config.findByIdAndRemove({_id: req.params.id}, (err, config) => {
-        if (err) {
-            res.json(err);
-        }
-        else {
-            res.json('Removed successfully');
-        }
-    });
-});
-
+// Delete a report by ID
 router.route('/reports/delete/:id').get((req, res) => {
     Report.findByIdAndRemove({_id: req.params.id}, (err, report) => {
         if (err) {
@@ -210,11 +104,3 @@ router.route('/reports/delete/:id').get((req, res) => {
         }
     });
 });
-
-// file writing
-function writeFiles(path, objToWrite) {
-    fs.writeFile(path, objToWrite, (err) => {
-        if (err) throw err;
-        console.log('Object written to ' + path);
-    });
-}
--- frontend/src/app/store/actions/index.ts
+++ frontend/src/app/store/actions/index.ts
@@ -1,1 +1,22 @@
 // Action Index
+import { Action } from '@ngrx/store';
+
+export enum Actions {
+  ReadAllReports = 'Read All Accounts',
+  ReadReportByID = 'Read Report By ID',
+  DeleteReportByID = 'Delete Report By ID',
+  CountFailedInReport = 'Count Failed In Report',
+  CountFailedInFixture = 'Count Failed In Fixture',
+  CountPassedInFixture = 'Count Passed In Fixture',
+  CountSkippedInFixture = 'Count Skipped In Fixture',
+  UpdateCurrentReportID = 'Update Current Report ID',
+  ReadAllFixtureFilenames = 'Read All Fixture Filenames',
+  WriteConfigFileToServer = 'Write Config File To Server',
+  ToggleHeadlessMode = 'Toggle Headless Mode',
+  ToggleAllBrowsers = 'Toggle All Browsers',
+  AddBrowserToConfig = 'Add Browser To Config',
+  RemoveBrowserFromConfig = 'Remove Browser From Config',
+  ToggleAllFixtures = 'Toggle All Fixtures',
+  AddFixtureToConfig = 'Add Fixture To Config',
+  RemoveFixtureFromConfig = 'Remove Fixture From Config'
+}
